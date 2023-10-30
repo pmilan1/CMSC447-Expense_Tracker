e250cbf0c9e7a42547bebb34e4e3cdb4e9fdee65
Milan Parmar <milan.parmar.0429@gmail.com>
2023-10-29 22:06:24 -0400

    Added sharing calculations and finished tab

--- app/src/main/java/com/example/rammzexpensetracker/ui/sharing/SharingFragment.java
+++ app/src/main/java/com/example/rammzexpensetracker/ui/sharing/SharingFragment.java
@@ -10,14 +10,41 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.rammzexpensetracker.R;
 import com.example.rammzexpensetracker.databinding.FragmentSharingBinding;
 
 public class SharingFragment extends Fragment {
 
-    private FragmentSharingBinding binding;
+    private EditText editValue;
+    private EditText totNumPeople;
+    private EditText tipPerc;
+    private Button calcButton;
+    private TextView resultTextView;
+
+    //private FragmentSharingBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_sharing, container, false);
+
+        editValue = root.findViewById(R.id.edit_value);
+        totNumPeople = root.findViewById(R.id.tot_num_people);
+        tipPerc = root.findViewById(R.id.tip_perc);
+        calcButton = root.findViewById(R.id.calc_button);
+        resultTextView = root.findViewById(R.id.result_text);
+
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateExpense();
+            }
+        });
+        return root;
+
+        /*
         SharingViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(SharingViewModel.class);
 
@@ -27,11 +54,33 @@ public class SharingFragment extends Fragment {
         final TextView textView = binding.textSharing;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+         */
     }
 
+    private void calculateExpense() {
+        String valueStr = editValue.getText().toString();
+        String numPeopleStr = totNumPeople.getText().toString();
+        String tipPercStr = tipPerc.getText().toString();
+
+        if (valueStr.isEmpty() || numPeopleStr.isEmpty()) {
+            resultTextView.setText("Please enter both Total and Number of People.");
+            return;
+        }
+
+        double value = Double.parseDouble(valueStr);
+        int numPeople = Integer.parseInt(numPeopleStr);
+        double tipPerc = tipPercStr.isEmpty() ? 0 : Double.parseDouble(tipPercStr);
+
+        double totalExpense = value + (value * tipPerc / 100);
+        double perPersonExpense = totalExpense / numPeople;
+
+        resultTextView.setText("Total Expense: $" + totalExpense + "\nPer Person: $" + perPersonExpense);
+    }
+/*
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+ */
 }
