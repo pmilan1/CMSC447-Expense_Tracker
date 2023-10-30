73c5c2cfa76a18e4a3261f0a5f4318eb60f578ea
Milan Parmar <milan.parmar.0429@gmail.com>
2023-10-30 07:46:49 -0400

    Cleaned up code and added comments

--- app/src/main/java/com/example/rammzexpensetracker/ui/sharing/SharingFragment.java
+++ app/src/main/java/com/example/rammzexpensetracker/ui/sharing/SharingFragment.java
@@ -24,37 +24,24 @@ public class SharingFragment extends Fragment {
     private Button calcButton;
     private TextView resultTextView;
 
-    //private FragmentSharingBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_sharing, container, false);
 
+        // attach variable to user input
         editValue = root.findViewById(R.id.edit_value);
         totNumPeople = root.findViewById(R.id.tot_num_people);
         tipPerc = root.findViewById(R.id.tip_perc);
         calcButton = root.findViewById(R.id.calc_button);
         resultTextView = root.findViewById(R.id.result_text);
 
-        calcButton.setOnClickListener(new View.OnClickListener() {
+        calcButton.setOnClickListener(new View.OnClickListener() {  // when button is clicked
             @Override
             public void onClick(View v) {
                 calculateExpense();
             }
         });
         return root;
-
-        /*
-        SharingViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(SharingViewModel.class);
-
-        binding = FragmentSharingBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSharing;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
-         */
     }
 
     private void calculateExpense() {
@@ -69,18 +56,11 @@ public class SharingFragment extends Fragment {
 
         double value = Double.parseDouble(valueStr);
         int numPeople = Integer.parseInt(numPeopleStr);
-        double tipPerc = tipPercStr.isEmpty() ? 0 : Double.parseDouble(tipPercStr);
+        double tipPerc = tipPercStr.isEmpty() ? 0 : Double.parseDouble(tipPercStr); // if empty, don't calculate
 
         double totalExpense = value + (value * tipPerc / 100);
         double perPersonExpense = totalExpense / numPeople;
 
         resultTextView.setText("Total Expense: $" + totalExpense + "\nPer Person: $" + perPersonExpense);
     }
-/*
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
- */
 }
