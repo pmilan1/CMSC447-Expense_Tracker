d4d3885747344dc11bfb5a746bb62497dcfac1ab
Melvin <melvinphilip2001@gmail.com>
2023-11-12 22:23:37 -0500

    Send the pie chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,9 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:2.7.4")
     implementation("androidx.navigation:navigation-ui:2.7.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
 }
