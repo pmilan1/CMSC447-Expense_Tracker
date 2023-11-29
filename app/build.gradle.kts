a9a3947b5f0adbcb1d25470e4dfe42213481c93b
rajjyarohan10 <rajjyarohan@gmail.com>
2023-11-29 10:24:09 -0500

    Implemented Material You

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,12 @@ dependencies {
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation("androidx.annotation:annotation:1.6.0")
+    // Material You Implementation
+    // Latest version as of Oct 4, 2023
+//    implementation("androidx.compose.material3:material3:material3:1.1.2")
+    // 2 yr old implementation method
+    implementation("com.google.android.material:material:1.5.0-beta01")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
